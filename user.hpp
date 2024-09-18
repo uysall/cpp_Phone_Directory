750c4f46734154c259ec5013805556ee228ce571
Ömer Sadık Uysal <omersadikuysal09@gmail.com>
2024-09-18 10:44:47 +0300

    Update user.hpp

--- user.hpp
+++ user.hpp
@@ -14,36 +14,39 @@
 
 struct User {
     std::string name, surname, email, phoneNumber;
+    std::vector<User> userList;
+
+    User() = default;
 
     User(std::string name, std::string surname, std::string email, std::string phoneNumber)
         : name(std::move(name)), surname(std::move(surname)), email(std::move(email)),
-          phoneNumber(std::move(phoneNumber)) {
-    }
+          phoneNumber(std::move(phoneNumber)) {}
 
-    void addUser(pqxx::connection &conn, const std::string &name, const std::string &surname, const std::string &email,
-                 const std::string &phoneNumber) {
+    void addUser(pqxx::connection &conn) const {
         pqxx::nontransaction nt(conn);
         nt.exec_params(
             "INSERT INTO direction.direction_table(name, surname, email, phonenumber) VALUES ($1, $2, $3, $4);",
             name, surname, email, phoneNumber);
     }
 
-    void removeUser(pqxx::connection &conn, const std::string &phoneNumber) {
+    void removeUser(pqxx::connection &conn) const {
         pqxx::nontransaction nt(conn);
         nt.exec_params("DELETE FROM direction.direction_table WHERE phonenumber = $1;", phoneNumber);
     }
 
-    std::string listUsers(pqxx::connection &conn) {
+    std::vector<User> listUsers(pqxx::connection &conn) {
         pqxx::nontransaction nt(conn);
         const pqxx::result res = nt.exec("SELECT name, surname, email, phonenumber FROM direction.direction_table;");
-        std::string output;
-        for (const auto &row: res) {
-            output += "Name: " + row["name"].as<std::string>() + ", ";
-            output += "Surname: " + row["surname"].as<std::string>() + ", ";
-            output += "Email: " + row["email"].as<std::string>() + ", ";
-            output += "Phone Number: " + row["phonenumber"].as<std::string>() + "\n";
+        std::vector<User> users;
+        for (const auto &row : res) {
+            users.emplace_back(
+                row["name"].as<std::string>(),
+                row["surname"].as<std::string>(),
+                row["email"].as<std::string>(),
+                row["phonenumber"].as<std::string>()
+            );
         }
-        return output;
+        return users;
     }
 };
 #endif
